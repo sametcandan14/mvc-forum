142887bc81dbd35aaa7aae0405d87a0d9e67454d
Samet Candan <c._samet@hotmail.com>
2024-03-10 22:28:34 +0300

    styling

--- src/pages/ListPosts/ListPostsView.jsx
+++ src/pages/ListPosts/ListPostsView.jsx
@@ -1,8 +1,15 @@
 const ListPostsView = ({ data }) => {
   return (
     <div className="container">
-      {data?.map(() => (
-        <div>Selam</div>
+      {!data && <p className="load">Yükleniyor...</p>}
+      {data?.map((post) => (
+        <div className="post" key={post.id}>
+          <div className="info">
+            <h3>{post.title}</h3>
+            <p>{post.user}</p>
+          </div>
+          <p className="text">{post.text}</p>
+        </div>
       ))}
     </div>
   );
